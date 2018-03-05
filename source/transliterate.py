36978d5c35da47f46fd73639cc6fba3e2e445913
Lili Szabó <lilasaba@gmail.com>
2018-03-05 16:51:57 +0100

    Fix errors in wordlist transliteration, change i/o handling

--- source/transliterate.py
+++ source/transliterate.py
@@ -45,25 +45,23 @@ class Transliterate:
                 if romanji:
                     outf.write('%s\t%s\n' % (romanji,pron))
 
-    def transliterate_wordlist(self,lexicon):
-        bname = '.'.join(os.path.basename(lexicon).split('.')[:-1])
+    def transliterate_wordlist(self,wordlist):
+        bname = '.'.join(os.path.basename(wordlist).split('.')[:-1])
         bname = '%s_romanji' % bname
         extension = 'words'
-        ## Create output directory, if it doesn't exist.
-        if not os.path.exists('../%s' % bname):
-            os.makedirs('../%s' % bname)
-        ## Define output lexicon name.
-        transliterated_lexicon = '../%s/%s.%s' % (bname,bname,extension) 
-        with open(lexicon) as inf, open(transliterated_lexicon,'wb') as outf:
+        pathname = os.path.dirname(wordlist)
+        ## Define output wordlist name.
+        transliterated_wordlist = '%s/%s.%s' % (pathname,bname,extension) 
+        with open(wordlist) as inf, open(transliterated_wordlist,'wb') as outf:
             for line in inf:
                 line = line.strip()
-                written,pron = line.split('\t')
+                written = line
                 romanji = self.transliterate_token(written)
                 ## Delete spaces from romanized token as it conflicts with
                 ## the g2p alignment.
                 romanji = romanji.replace(' ','')
                 if romanji:
-                    outf.write('%s\t%s\n' % (romanji,pron))
+                    outf.write('%s\n' % romanji)
 
 if __name__ == '__main__':
     lexicon = sys.argv[1]
