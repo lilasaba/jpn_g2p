6decfbd8a20e1585a964dd024aa848c19ded120e
Lili Szabó <lilasaba@gmail.com>
2018-03-05 16:34:59 +0100

    Process wordlist with espeak word2pron converter

--- source/wlist2lex.py
+++ source/wlist2lex.py
@@ -6,19 +6,59 @@ import sys
 from subprocess import check_output
 
 class Word2Pron:
+    '''
+    Convert written form to phonemic form (IPA) with espeak.
+    Dependencies: espeak (https://github.com/espeak-ng/espeak-ng/blob/master/README.md).
+    Convert string (word2ipa) or lexicon (in2out).
+    How to run:
+    >>> python wlist2lex.py <path/to/wordlist.words>
+    Output:
+        <path/to/wordlist.train>
+    '''
     def __init__(self):
-        pass
+        self.espeak_phoneme_set = set()
+        self.wikt_phoneme_set = {'ɾ','a','b','d̪','ɛ','h','i','j',
+                'k','m','n̪','ɔ','p','s̪','t̪','w','z̪'}
 
     def word2ipa(self,word):
         pron = check_output(["espeak", "-q","--ipa",
+            '--sep=|',
             '-v','ja',
             word]).decode('utf-8')
-        print(pron)
+        return pron
+
+    def remove_espeak_markup(self,pron):
+        pron = pron.replace('(en)','')
+        pron = pron.replace('(ja)','')
+        pron = pron.replace(' ','')
+        ## Remove accent (marked by u'\u02c8').
+        pron = pron.replace(chr(712),'')
+        ## 
+        pron = pron.replace('|',' ')
+        pron = pron.strip()
+
+        return pron
 
     def in2out(self,wlist):
-        pass
+        out_name = wlist.replace('.words','_espeak.train')
+        with open(wlist,encoding='utf-8') as wordlist,\
+                open(out_name,'w',encoding='utf-8') as lexicon:
+            for word in wordlist: 
+                word = word.strip()
+                pron = self.word2ipa(word)
+                pron = self.remove_espeak_markup(pron)
+                if pron:
+                    #print(word,pron)
+                    for ph in pron.split(' '):
+                        self.espeak_phoneme_set.add(ph)
+                    lexicon.write('%s\t%s\n' % (word,pron))
+
+        print(self.espeak_phoneme_set)
+        print(self.espeak_phoneme_set & self.wikt_phoneme_set)
+        print(self.espeak_phoneme_set - self.wikt_phoneme_set)
+        print(self.wikt_phoneme_set - self.espeak_phoneme_set)
 
 if __name__ == '__main__':
-    word = '少女'
+    wordlist = sys.argv[1]
     jpn_w2p = Word2Pron()
-    jpn_w2p.word2ipa(word)
+    jpn_w2p.in2out(wordlist)
